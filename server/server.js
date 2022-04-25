b3aa98f51327ff3a10e60dbaac70efd1e68417d1
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-04-25 20:25:00 +0900

    deploy2

--- server/server.js
+++ server/server.js
@@ -4,4 +4,4 @@ const app = express();
 app.get('/', function(req, res){
     res.send('hello NodeJs');
 })
-app.listen(3000, () => console.log('3000번 포트 대기'));
+app.listen(80, () => console.log('80번 포트 대기'));
