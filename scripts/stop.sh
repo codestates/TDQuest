af8cfe57197f8efe1b90c938382a6c6d95d8073f
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-04-25 22:43:26 +0900

    deply5

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/im-sprint-practice-deploy/server
-pm2 stop app.js 2> /dev/null || true
-pm2 delete app.js 2> /dev/null || true
+cd /home/ubuntu/TDQuest/server
+pm2 stop server.js 2> /dev/null || true
+pm2 delete server.js 2> /dev/null || true
