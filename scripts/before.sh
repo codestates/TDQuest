5feb13017bd086e3758fc4be0464cbc891680522
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-04-27 00:26:53 +0900

    test https3

--- scripts/before.sh
+++ scripts/before.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-<<<<<<< HEAD
 if [ -d /home/ubuntu/TDQuest/ ]; then
     rm -rf /home/ubuntu/TDQuest
 fi
 mkdir -vp /home/ubuntu/TDQuest
-=======
-if [ -d /home/ubuntu/TDQuest ]; then
-    rm -rf /home/ubuntu/TDQuest
-fi
-mkdir -vp /home/ubuntu/TDQuest
->>>>>>> router
