53a24e5c72ce774991366923dc9ec78b156017af
junjun0103 <lucky.hyunjun@gmail.com>
2022-05-06 22:11:10 +1200

    [sprint1] 헤더 토글메뉴 기능 추가, responsive design 추가

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,9 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const FooterContainer = styled.footer<{ bgColor: string }>`
   background-color: ${(props) => props.bgColor};
   height: 150px;
-  display: ;
 `;
 
 const Description = styled.section`
