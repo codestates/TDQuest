1c7c9a4dad75a4d0d0d997aa66845c9f75237cf4
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-05-09 00:50:02 +0900

    modify Login,Auth

--- server/controllers/sign.js
+++ server/controllers/sign.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const jwt = require("jsonwebtoken");
 const { user } = require("../models");
 
@@ -12,6 +13,32 @@ module.exports = {
     });
     res.status(200).redirect("/login");
   },
+=======
+const jwt = require('jsonwebtoken')
+const { user } = require('../models')
+const { character } = require("../models")
+const { makeAccessToken } = require('../middleware/auth');
+
+module.exports = {
+    signIn : async (req, res) => {
+        const { email, nickname, password } = req.body.userInfo
+
+        const passwordToken = makeAccessToken(password)
+        const userInfo = user.create({
+            email: email,
+            nickname: nickname,
+            password: passwordToken
+        })
+
+        await user.findOne({ where : {
+            email : req.body.email
+        }})
+        .then(data => {
+            const characterInfo = character.create()
+            res.status(200).redirect('/login').json({characterInfo: characterInfo})
+        })
+    },
+>>>>>>> 07a5d80 (create Redis)
 
   signOut: async (req, res) => {
     user.destory({ where: { id: req.query.id } });
