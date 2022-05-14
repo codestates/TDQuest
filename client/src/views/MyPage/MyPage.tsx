c7196eff9a0309b2dfbd2ae6a18bb774b60949b6
MinjunShin <mj.irin1260@gmail.com>
2022-05-15 00:13:44 +0900

    mypage 중간저장

--- client/src/views/MyPage/MyPage.tsx
+++ client/src/views/MyPage/MyPage.tsx
@@ -6,6 +6,7 @@ import {
   fontSize_body_laptop,
   color_white,
   color_context_beige,
+  color_context_beige_light,
 } from "../../components/CommonStyle";
 import Loading from "../../components/Loading";
 import Status from "../../components/Status";
@@ -13,7 +14,12 @@ import HelperBear from "../../components/HelperBear";
 import Button from "../../components/Button";
 import MsgModal from "../../components/MsgModal";
 import DeleteUserAlert_Modal from "./DeleteUserAlert_Modal";
-import { CharDataType, UserDataType } from "../../Types/generalTypes";
+import DoneContents from "./DoneContents";
+import {
+  CharDataType,
+  UserDataType,
+  TodoListType,
+} from "../../Types/generalTypes";
 import {
   MyPageContainer,
   MyPageHeader,
@@ -21,30 +27,37 @@ import {
   CharContainer,
   UserInfoDetailContainer,
   HelperBearContainer,
+  BottomContentContainer,
+  MyDoneListContainer,
+  AchievementsContainer,
+  TitleContainer,
+  ContentContainer,
 } from "./MyPageStyle";
 // API REQUEST
 import {
   dummyRes_getCharacterInfo,
+  dummyRes_getTodolist,
   dummyRes_getUserInfo,
 } from "../../API/tdquestAPI";
 
 function MyPage() {
   const [charData, setCharData] = useState<CharDataType>({} as CharDataType);
   const [userData, setUserData] = useState<UserDataType>({} as UserDataType);
+  const [donelist, setDonelist] = useState<TodoListType[]>([]);
   const [loading, setLoading] = useState(true);
   const [onChange, setOnChange] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
     if (loading) {
-      setTimeout(() => {
-        const getcharacterData: any =
-          dummyRes_getCharacterInfo.data.characterInfo;
-        const getUserData: any = dummyRes_getUserInfo.data.userInfo;
-        setUserData(getUserData);
-        setCharData(getcharacterData);
-        setLoading(false);
-      }, 500);
+      const getcharacterData: any =
+        dummyRes_getCharacterInfo.data.characterInfo;
+      const getUserData: any = dummyRes_getUserInfo.data.userInfo;
+      const donelists: any = dummyRes_getTodolist.todoInfo;
+      setDonelist(donelists);
+      setUserData(getUserData);
+      setCharData(getcharacterData);
+      setLoading(false);
     }
   }, []);
 
@@ -76,56 +89,6 @@ function MyPage() {
     console.log("유저 정보 삭제");
   };
 
-  const BottomContentContainer = styled.div`
-    display: flex;
-    width: 90%;
-    height: 500px;
-    margin-top: 15px;
-    justify-content: space-between;
-  `;
-
-  const MyDoneListContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 48%;
-  `;
-
-  const AchievementsContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 48%;
-  `;
-
-  const TitleContainer = styled.div`
-    width: 100%;
-    height: 45px;
-    background-color: ${color_context_brown};
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    h3 {
-      font-size: ${fontSize_body_laptop};
-      font-family: "Fredoka One", cursive;
-      color: ${color_white};
-    }
-  `;
-
-  const ContentContainer = styled.div`
-    display: grid;
-    width: 100%;
-    height: 90%;
-    grid-template-columns: 0.9fr 1fr;
-    align-self: center;
-    justify-self: center;
-    background-color: ${color_context_beige};
-    h3 {
-      font-size: ${fontSize_body_laptop};
-      height: 1.5rem;
-      font-family: "OpenSans";
-      text-align: center;
-    }
-  `;
-
   return (
     <div>
       {loading ? (
@@ -156,9 +119,17 @@ function MyPage() {
               <Status charData={charData} onlyChar={true} />
             </CharContainer>
             <UserInfoDetailContainer>
-              <div className="user_id_wrapper">
-                <h1>{user_id}</h1>
-              </div>
+              {onChange ? (
+                <input
+                  type="text"
+                  className="change_name"
+                  placeholder={user_id}
+                ></input>
+              ) : (
+                <div className="user_id_wrapper">
+                  <h1>{user_id}</h1>
+                </div>
+              )}
               <h2>{email}</h2>
               {!onChange ? (
                 <div className="ButtonContainer">
@@ -186,7 +157,17 @@ function MyPage() {
               <TitleContainer>
                 <h3>My Done Lists</h3>
               </TitleContainer>
-              <ContentContainer></ContentContainer>
+              <ContentContainer>
+                {donelist.map((el, idx) => {
+                  return (
+                    <DoneContents
+                      key={idx}
+                      content={el.content}
+                      created_at={el.created_at}
+                    />
+                  );
+                })}
+              </ContentContainer>
             </MyDoneListContainer>
             <AchievementsContainer>
               <TitleContainer>
@@ -202,3 +183,11 @@ function MyPage() {
 }
 
 export default MyPage;
+
+// 필요 데이터
+{
+  // user info - user name, email, password
+  // user info에 total done lists(총 갯수만) 추가 필요
+  // user todo done lists
+  // done todo_list 삭제 요청
+}
