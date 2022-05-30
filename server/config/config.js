bf6991d164c552e402024819c9dbaf7d83827a0a
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-05-30 13:30:26 +0900

    [sprint2] ready deploy

--- server/config/config.js
+++ server/config/config.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: "root", //process.env.DATABASE_USER,
-    password: "", //process.env.DATABASE_PASSWORD,
-    database: "tdquest", //process.env.DATABASE_NAME,
-    host: "127.0.0.1", //process.env.DATABASE_HOST,
+    username: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: process.env.DATABASE_HOST,
     dialect: "mysql",
     timezone: "+09:00",
   },
