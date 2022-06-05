1587d5d91473332d6ab3a58e820bd999ff900ccf
tastestar91 <84552632+tastestar91@users.noreply.github.com>
2022-06-05 21:34:49 +0900

    modfiy

--- server/server.js
+++ server/server.js
@@ -5,17 +5,14 @@ const router = require("./router");
 const https = require("https");
 const fs = require("fs");
 const cookieParser = require("cookie-parser");
-const address = ["https://d2lx9yleo3gz1b.cloudfront.net", "https://tdquest.ml"]
-const corsOptions = {
-  origin: function(origin, callback){
-  const list = address.indexOf(origin) !== -1;
-  callback(null, list); 
-  },
-  credentials:true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
-}
 
-app.use(cors(corsOptions))
+
+app.use(cors({
+  origin : ["https://d2lx9yleo3gz1b.cloudfront.net", "https://tdquest.ml"],
+  credentials : true,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
+}))
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
