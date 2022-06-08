569e9c22db480af5e23898a703a7383b86170812
junjun0103 <lucky.hyunjun@gmail.com>
2022-06-08 12:57:43 +1200

    merge

--- client/src/API/tdquestAPI.ts
+++ client/src/API/tdquestAPI.ts
@@ -6,7 +6,7 @@ axios.defaults.headers.common['Authorization'] = `Bearer ${JSON.parse(
 
 // env 파일에서 url 주소를 불러오도록 설정할 예정
 export const APIMAIN = 'https://tdquest.tk';
-// export const APIMAIN = "http://localhost:3001";
+// export const APIMAIN = 'http://localhost:3001.tk';
 
 export const LOCALSTORAGE_STRING = JSON.parse(
   window.localStorage.getItem('isLogin') || '{}'
